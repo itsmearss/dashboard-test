582c91a88b62935e0f95dcd9d3aed291ef61b105
itsmearss <annurriyadhus17@gmail.com>
2024-07-25 21:02:46 +0700

    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,6 +3,6 @@
         Copyright &copy; 2024 <div class="bullet"></div> All Right Reserved</a>
     </div>
     <div class="footer-right">
-        Clinic V1.0
+        Annur
     </div>
 </footer>
